29ddfdb8b8d4ffd5067388bd8a6e35fb6896fbd1
vidhvath28 <vidhvathj.2005@gmail.com>
2024-05-07 21:13:24 +0530

    Day37 Creating Profile_page-02

--- Meta-NFT/src/Pages/Account.jsx
+++ Meta-NFT/src/Pages/Account.jsx
@@ -10,6 +10,18 @@ import Form from '../AccountPage/Form/Form'
 
 const Account = () => {
     const [fileUrl, setFileUrl] = useState(null);
+
+    const onDrop = useCallback(async(acceptedFile)=> {
+        setFileUrl(acceptedFile[0]);
+
+    }, []);
+
+
+    const {getRootProps, getInputProps} =  useDropzone({
+        onDrop,
+        accept: "image/*",
+        maxSize: 5000000,
+    })
   return (
     <div className={Style.account}>
         <div className={Style.account_info}>
@@ -17,8 +29,8 @@ const Account = () => {
             <p>You can set preferred display name, create your own NFT and can have your personal settings</p>
         </div>
         <div className={Style.account_box}>
-            <div className={Style.account_box_img}>
-                <input />
+            <div className={Style.account_box_img} {...getRootProps()}>
+                <input {...getInputProps()}/>
                 <img src={images.user1} alt="account upload" width={150} height={150} className={Style.account_box_img_img}/>
             </div>
             <div className={Style.account_box_from}></div>
