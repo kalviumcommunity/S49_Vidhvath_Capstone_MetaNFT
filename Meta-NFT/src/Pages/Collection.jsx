44e154d81a1f60bc33392cf80288dcacb822562d
vidhvath28 <vidhvathj.2005@gmail.com>
2024-05-07 10:51:28 +0530

    Day34 Creating CollectionPage-02

--- Meta-NFT/src/Pages/Collection.jsx
+++ Meta-NFT/src/Pages/Collection.jsx
@@ -3,16 +3,34 @@ import React from 'react'
 //INTERNAL IMPORT
 import Style from '../styles/Collection.module.css'
 import images from '../img'
-import {Banner, CollectionProfile} from '../CollectionPage/Collectionindex'
+import {Banner, CollectionProfile, NFTCardtwo} from '../CollectionPage/Collectionindex'
 import {Slider, Brand} from '../components/componentsindex'
 import Filter from '../components/Filter/Filter'
 
 const Collection = () => {
+
+  const collectionArray = [
+    images.nft_image_1,
+    images.nft_image_2,
+    images.nft_image_3,
+    images.nft_image_1,
+    images.nft_image_2,
+    images.nft_image_3,
+    images.nft_image_1,
+    images.nft_image_2,
+    images.nft_image_3,
+    images.nft_image_1,
+    images.nft_image_2,
+    images.nft_image_3,
+  ]
   return (
     <div className={Style.collection}>
         <Banner BannerImage={images.creatorbackground1} />
-        <CollectionProfile/>
+        <CollectionProfile />
+        <NFTCardtwo NFTData={collectionArray}/>
         <Filter/>
+
+        <Brand/>
     </div>
   )
 }
