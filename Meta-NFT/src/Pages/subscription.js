010e0d324a31580845bba645e615873eadb07cbc
vidhvath28 <vidhvathj.2005@gmail.com>
2024-05-13 10:23:45 +0530

    Day47 Creating Subcription Page-01

---
+++ Meta-NFT/src/Pages/subscription.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+
+//INTERNAL IMPORT
+import Style from '../styles/subscription.module.css'
+import Subscription from '../Subscription/Subscription'
+
+const subscription = () => {
+  const subscriptionArray = [
+    {
+      plan: "STARTER",
+      price: "$5/mo",
+      popular: "",
+      service: ["Automated Reporting", "Faster Processing", "Customizations"],
+      info: "Literally you probably haven't heard of them jean shorts.",
+
+    },
+    {
+      plan: "STARTER",
+      price: "$5/mo",
+      popular: "",
+      service: ["Automated Reporting", "Faster Processing", "Customizations"],
+      info: "Literally you probably haven't heard of them jean shorts.",
+    }
+  ]
+  return (
+    
+    <div className={Style.subscription}>
+      <div className={Style.Subscription_box}>
+        <div className={Style.Subscription_box_info}>
+          <h1> Subscription</h1>
+          <p>Pricing to fit the needs of any companie size.</p>
+        </div>
+        <div className={Style.Subscription_box_box}>
+          {subscriptionArray.map((el,i)=>(
+            <Subscription key={i + 1} i={1}/>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default subscription
---
+++ Meta-NFT/src/Subscription/Subscription.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//INTERNAL IMPORT
+import Style from '../styles/Subscription.module.css'
+
+const Subscription = () => {
+  return (
+    <div>Subscription</div>
+  )
+}
+
+export default Subscription
