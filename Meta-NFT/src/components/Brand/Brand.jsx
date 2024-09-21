493338c54ed1ee21991232f0a20f912c5fd15526
vidhvath28 <vidhvathj.2005@gmail.com>
2024-09-21 16:22:30 +0530

    css stying added

--- Meta-NFT/src/components/Brand/Brand.jsx
+++ Meta-NFT/src/components/Brand/Brand.jsx
@@ -11,17 +11,17 @@ const Brand = () => {
       <div className={Style.Brand_box}>
         <div className={Style.Brand_box_left}>
           <img src={images.logo1} alt="brand logo" width={100} height={100} />
-          <h1>Earn free crypto with MetaNFt</h1>
+          <h1>Earn free crypto with MetaNFT</h1>
           <p>A creative agency that leads and inspires.</p>
 
           <div className={Style.Brand_box_left_btn}>
-            <Button btnName="Create" handleClick={() => {}} />
-            <Button btnName="Discover" handleClick={() => {}} />
+            <Button btnName="Create" className="createButton" handleClick={() => {}} />
+            <Button btnName="Discover" className="discoverButton" handleClick={() => {}} />
           </div>
         </div>
 
         <div className={Style.Brand_box_right}>
-          <img src={images.earn} alt="brand logo" width={800} height={800} />
+          <img src='https://th.bing.com/th/id/OIP.CXBZodOHZwy5uSxgaYW8TQHaEK?rs=1&pid=ImgDetMain' alt="brand logo" width={800} height={800} />
         </div>
       </div>
     </div>
--- Meta-NFT/src/components/Video/Video.jsx
+++ Meta-NFT/src/components/Video/Video.jsx
@@ -1,10 +1,9 @@
-// Video.jsx
-
 import React from 'react';
 import Style from './Video.module.css';
-import images from '../../img';
 
 const Video = () => {
+  const videoUrl = 'https://youtu.be/mImtEXf79Jk?si=7mrN-Q3OTDmfVGyJ'; // Replace with your video URL
+
   return (
     <div className={Style.Video}>
       <div className={Style.Videobox}>
@@ -12,14 +11,16 @@ const Video = () => {
         <p>Check out our trending videos. View more perspectives on just about any topic.</p>
         <div className={Style.Video_box_frame}>
           <div className={Style.Video_box_frame_left}>
-            <img src={images.video_image} alt="Video Image" />
+            <img src='https://media3.giphy.com/media/Xnqt96afMpn4Ivfge4/giphy.gif?cid=790b76113886900d90d7aa78065930dfb79ffdfba815587e&rid=giphy.gif&ct=g' alt="Video Image" />
           </div>
           <div className={Style.Video_box_frame_right}>
             <h2>Explore More Videos</h2>
             <p>
               Discover a wide range of perspectives and insights on various topics from our community.
             </p>
+            <a href={videoUrl} className={Style.watchNowButton} >
             <button>Watch Now</button>
+            </a>
           </div>
         </div>
       </div>
